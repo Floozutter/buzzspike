5513f825c87bbe799c642466f2143e3f9fea76df
Floozutter <me@floozutter.site>
2021-06-10 02:31:47 -0700

    implement uoic

--- bzsp/recog.py
+++ bzsp/recog.py
@@ -1,7 +1,21 @@
 import cv2
 import numpy
+from numpy import ndarray
 
-def union_of_intersecting_components(mask_a, mask_b):
-    intersection = cv2.bitwise_and(mask_a, mask_b)
-    union = numpy.zeros_like(intersection)
-    _, labels, stats, _ = cv2.connectedComponentsWithStats(...)
+def union_of_intersecting_components(a_bimg: ndarray, b_bimg: ndarray) -> ndarray:
+    # find the intersecting labels (labels for components that participate in intersections)
+    inter = cv2.bitwise_and(a_bimg, b_bimg)
+    n, _, stats, _ = cv2.connectedComponentsWithStats(inter)
+    _, a_labeled = cv2.connectedComponents(a_bimg)
+    _, b_labeled = cv2.connectedComponents(b_bimg)
+    a_intersecting_labels: set[int] = set()
+    b_intersecting_labels: set[int] = set()
+    for inter_label in range(1, n):
+        top, left = stats[inter_label, cv2.CC_STAT_TOP], stats[inter_label, cv2.CC_STAT_LEFT]
+        a_intersecting_labels.add(a_labeled[top, left])
+        b_intersecting_labels.add(b_labeled[top, left])
+    # use the intersecting labels to make masks that describe where to bitwise_or
+    a_where = numpy.isin(a_labeled, list(a_intersecting_labels))
+    b_where = numpy.isin(b_labeled, list(b_intersecting_labels))
+    where = a_where | b_where
+    return numpy.bitwise_or(a_bimg, b_bimg, out = numpy.zeros_like(inter), where = where)
--- tests/test_recog.py
+++ tests/test_recog.py
@@ -1,17 +1,19 @@
 from bzsp import recog
 import cv2
 import numpy
-from numpy.typing import ArrayLike
+from numpy import ndarray
 
-def read_mask(path: str) -> ArrayLike:
+def read_bimg(path: str) -> ndarray:
     image = cv2.imread(path)
     grayscaled = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, threshed = cv2.threshold(grayscaled, 1, 255, cv2.THRESH_BINARY)
     return threshed
 
 def test_uoic() -> None:
-    mask_a = read_mask("tests/test_recog/uoic_a.png")
-    mask_b = read_mask("tests/test_recog/uoic_b.png")
-    uoiced = recog.union_of_intersecting_components(mask_a, mask_b)
-    n, _, _, _ = cv2.connectedComponentsWithStats(uoic, 4, cv2.CV_32S)
-    assert n == 2
+    a_bimg = read_mask("tests/test_recog/uoic_a.png")
+    b_bimg = read_mask("tests/test_recog/uoic_b.png")
+    uoiced = recog.union_of_intersecting_components(a_bimg, b_bimg)
+    n, _, _, _ = cv2.connectedComponentsWithStats(uoiced, 4, cv2.CV_32S)
+    cv2.imshow("uoiced", uoiced)
+    cv2.waitKey(0)
+    assert n == 3
