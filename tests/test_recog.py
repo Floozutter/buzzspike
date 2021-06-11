17b0bf606e8805fc088bf4a7a425e82688e112f6
Floozutter <me@floozutter.site>
2021-06-11 03:15:24 -0700

    discover uoic bug with uoic_c.png and uoic_d.png

--- tests/test_recog.py
+++ tests/test_recog.py
@@ -9,11 +9,20 @@ def read_bimg(path: str) -> ndarray:
     _, threshed = cv2.threshold(grayscaled, 1, 255, cv2.THRESH_BINARY)
     return threshed
 
-def test_uoic() -> None:
-    a_bimg = read_bimg("tests/test_recog/uoic_a.png")
-    b_bimg = read_bimg("tests/test_recog/uoic_b.png")
-    uoiced = recog.union_of_intersecting_components(a_bimg, b_bimg)
-    n, _, _, _ = cv2.connectedComponentsWithStats(uoiced, 4)
-    cv2.imshow("uoiced", uoiced)
-    cv2.waitKey(0)
-    assert n == 3
+class TestUoic:
+    def test_ab(self) -> None:
+        a_bimg = read_bimg("tests/test_recog/uoic_a.png")
+        b_bimg = read_bimg("tests/test_recog/uoic_b.png")
+        uoiced = recog.union_of_intersecting_components(a_bimg, b_bimg)
+        n, _, _, _ = cv2.connectedComponentsWithStats(uoiced, 4)
+        cv2.imshow("TestUoic.test_ab: uoiced", uoiced)
+        cv2.waitKey(0)
+        assert n == 3
+    def test_cd(self) -> None:
+        c_bimg = read_bimg("tests/test_recog/uoic_c.png")
+        d_bimg = read_bimg("tests/test_recog/uoic_d.png")
+        uoiced = recog.union_of_intersecting_components(c_bimg, d_bimg)
+        n, _, _, _ = cv2.connectedComponentsWithStats(uoiced, 4)
+        cv2.imshow("TestUoic.test_cd: uoiced", uoiced)
+        cv2.waitKey(0)
+        assert n == 2
